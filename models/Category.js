eb634e6096656ea4c4f632f046f7767e1d82c0a2
mathminx <122234007+mathminx@users.noreply.github.com>
2023-04-23 18:46:41 -0300

    built models and routes

--- models/Category.js
+++ models/Category.js
@@ -1,23 +1,32 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Category extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
+Category.init(
   {
-    // define columns
+    category_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'category',
   }
 );
 
-module.exports = Product;
+module.exports = Category;
--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,9 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+  
   console.log('\n----- DATABASE SYNCED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
